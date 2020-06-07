2b96a6be13b8472485ffb7e3369894503bec49ab
Robert Quintana <robertquintanatx@gmail.com>
2020-06-07 22:34:11 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(chosen_name)
+   @name = chosen_name
+  end 
+  
+  def name
+    @name
+  end
+  
+  def job=(chosen_job)
+    @job = chosen_job
+  end 
+  
+  def job
+    @job
+  end
+  
+end 
