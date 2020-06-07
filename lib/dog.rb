06fd6d2ed86202fc78e4ad4c7f3c3a14053b8bd9
Robert Quintana <robertquintanatx@gmail.com>
2020-06-07 22:23:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(dog_name)
+    @inst_dog_name = dog_name  
+  end 
+  
+  def name 
+    @inst_dog_name
+  end 
+  
+  def breed=(breed_name)
+    @inst_breed_name = breed_name
+  end 
+  
+  def breed 
+    @inst_breed_name
+  end
+  
+end 
